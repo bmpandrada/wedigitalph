e3f5f05ceadd3d90434949d0b9e4c636a25bf996
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-04-13 17:00:25 +0800

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logoNav from '../assets/images/logo/WeDigital-PH-Logo.png'
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+
+  const handleClick = () => {
+    setToggle(!toggle)
+  }
   return (
     <div className="navbar">
     <div className="logo-container">
       <img className='logo-img' src={logoNav} alt="" />
     </div>
-    <nav>
-      <ul>
-        <li><a className='active' href="#">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Services <ion-icon name="caret-down-outline"></ion-icon></a>
+    <nav >
+      <ul className={`${toggle ? 'show' : ''}`}>
+        <li><a onClick={handleClick} className='active' href="#">Home</a></li>
+        <li><a onClick={handleClick} href="#">About Us</a></li>
+        <li><a onClick={handleClick} href="#">Services <ion-icon name="caret-down-outline"></ion-icon></a>
         <ul className='sub'>
-                <li><a href="">Digital Marketing</a></li>
-                <li><a href="">Web Design & Development</a></li>
-                <li><a href="">Hosting</a></li>
+                <li><a onClick={handleClick} href="">Digital Marketing</a></li>
+                <li><a onClick={handleClick} href="">Web Design & Development</a></li>
+                <li><a onClick={handleClick} href="">Hosting</a></li>
             </ul></li>
         <li><a href="#">Oracle</a></li>
         <li><a href="#">Blog</a></li>
       </ul>
+      <div className="burger" onClick={handleClick}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </nav>
     <div className="nav-btn">
     <a href="" className='btn'>Get Qoutes</a>
